02d7194f72bf4acf7e361fda322565c6d8724f95
ojh <nbczerg98@naver.com>
2023-09-06 11:46:10 +0900

    Wed Sep  6 11:46:10     2023    by juhyun

--- SpringMVC01/src/main/java/kr/spring/controller/BoardController.java
+++ SpringMVC01/src/main/java/kr/spring/controller/BoardController.java
@@ -1,15 +1,23 @@
 package kr.spring.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.spring.entity.Board;
+import kr.spring.mapper.BoardMapper;
 
 @Controller // 현재 클래스를 핸들러맵핑이 찾기위해 컨트롤러로 등록하는 부분
 public class BoardController {
+	
+	@Autowired
+	private BoardMapper mapper; // MyBatis한테 JDBC 실행하게 요청하는 객체
+	
+	
 	
 	/*
 	 * @RequestMapping("/") // 요청 url로 들어왔을때 아래 기능을 수행하겠다 public String home() {
@@ -22,18 +30,8 @@ public class BoardController {
 		
 		// 게시글 정보 가져오기
 		// 한 개의 게시글은 - 번호, 제목, 내용, 작성자, 작성일, 조회수
-		Board b1 = new Board(1, "오늘 점심 추천 받는다", "짜장면 말고..", "중국집매니아", "2023-09-05", 5);
-		Board b2 = new Board(2, "어제 마지막 나간사람 누구임?", "정말 대단하네!", "준용쌤", "2023-09-05", 8);
-		Board b3 = new Board(3, "치우야 거기있는 물건좀 치우라", "ㅋㅋㅋ 재밌징", "김태연", "2023-09-05", 11);
-		Board b4 = new Board(4, "오늘 단축수업으로 인해 오늘 수업은 오전만 진행합니다", "힝! 속았찌?", "명훈쌤", "2023-09-05", 1);
-		Board b5 = new Board(5, "오늘까지해서 우리반 총 7point 획득", "진짜 5만원 인당 가져가냐 이거?", "병관쌤", "2023-09-05", 45);
-		
-		ArrayList<Board> list = new ArrayList<Board>();
-		list.add(b1);
-		list.add(b2);
-		list.add(b3);
-		list.add(b4);
-		list.add(b5);
+		// 전체 게시글 조회기능
+		List<Board> list = mapper.getLists();
 		
 		// 객체바인딩 - 동적바인딩
 		model.addAttribute("list", list);
