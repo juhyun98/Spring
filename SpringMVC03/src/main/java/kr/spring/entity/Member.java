785351ec715bb0c6e9918f27e22ee15f255540dc
ojh <nbczerg98@naver.com>
2023-09-14 12:48:54 +0900

    Thu Sep 14 12:48:53     2023    by juhyun

---
+++ SpringMVC03/src/main/java/kr/spring/entity/Member.java
@@ -0,0 +1,26 @@
+package kr.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor // 기본생성자
+@ToString
+public class Member {
+	
+	private int memIdx;
+	private String memID;
+	private String memPassword;
+	private String memName;
+	private int memAge;
+	private String memGender;
+	private String memEmail;
+	private String memProfile;
+	
+	
+	
+
+}
