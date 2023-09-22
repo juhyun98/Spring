a8d29eacfece4623743c92a88963724e4ac4b5de
ojh <nbczerg98@naver.com>
2023-09-22 10:28:27 +0900

    Fri Sep 22 10:28:27     2023    by juhyun

--- SpringMVC04/src/main/java/kr/spring/config/SecurityConfig.java
+++ SpringMVC04/src/main/java/kr/spring/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration // WebConfig.java에서 SecurityConfig를 읽어오기위한 어노테이션
 @EnableWebSecurity // web에서 Sercurity를 쓰겠다
@@ -15,6 +17,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// 요청에 대한 보안 설정하는 곳
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		http.addFilterBefore(filter, CsrfFilter.class);
 		
 	}
 	
--- SpringMVC04/src/main/webapp/WEB-INF/views/member/joinForm.jsp
+++ SpringMVC04/src/main/webapp/WEB-INF/views/member/joinForm.jsp
@@ -23,7 +23,12 @@
 	    <div class="panel-body">
 
 		<form action="${contextPath}/join.do" method="post">
-		
+			<!-- 
+				이제는 서버로 단순히 회원가입 정보만 전달하는 것이 아니라
+				서버에서 발행한 토큰도 전달해야 회원가입이 가능하다
+			 -->
+			 <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}">
+			 
 			<input type="hidden" name="memPassword" id="memPassword" value="" >
 		
 			<table style="text-align: center; border : 1px solid #dddddd" class="table table-bordered">
