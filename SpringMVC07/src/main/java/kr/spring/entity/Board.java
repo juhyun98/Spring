ff443297fc2e3e9db3342d4f21da848d2fa0cd79
ojh <nbczerg98@naver.com>
2023-10-04 12:05:09 +0900

    Wed Oct  4 12:05:08     2023    by juhyun

--- SpringMVC07/src/main/java/kr/spring/entity/Board.java
+++ SpringMVC07/src/main/java/kr/spring/entity/Board.java
@@ -2,6 +2,11 @@ package kr.spring.entity;
 
 public class Board {
 	
-	
-	
+	private int idx;
+	private String memID;
+	private String title;
+	private String content;
+]	private String writer;
+]	private String writer;
+
 }
