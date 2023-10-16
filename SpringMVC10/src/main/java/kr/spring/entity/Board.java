df0ad751298f6136f9ba62de928d9a167839caf9
ojh <nbczerg98@naver.com>
2023-10-16 12:21:06 +0900

    Mon Oct 16 12:21:06     2023    by juhyun

--- SpringMVC10/src/main/java/kr/spring/entity/Board.java
+++ SpringMVC10/src/main/java/kr/spring/entity/Board.java
@@ -1,11 +1,28 @@
 package kr.spring.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Entity // Board VO가 Database Table로 만들때 설정
 @Data
-public class Board {
+public class Board { // VO <--- ORM ----> TABLE
+	
+	@Id // PK 의미	 // 1씩 증가하면서 넣기 -> auto_incremente와 같음
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idx; // 게시글 고유번호 (호환을 long형 해준다)
+	
+	private String title;
+	
+	@Column(length = 2000) // 길이지정 -> 길이지정 따로 안할때 길이 255
+	private String content;
 	
 }
+
+
+
+
