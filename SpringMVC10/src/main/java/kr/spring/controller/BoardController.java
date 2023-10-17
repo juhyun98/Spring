8c538947dc678cc16097af68c37ced003e550b52
ojh <nbczerg98@naver.com>
2023-10-17 12:17:41 +0900

    Tue Oct 17 12:17:40     2023    by juhyun

---
+++ SpringMVC10/src/main/java/kr/spring/controller/BoardController.java
@@ -0,0 +1,35 @@
+package kr.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.spring.entity.Board;
+import kr.spring.service.BoardService;
+
+@Controller
+public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping("/list")
+	public String list(Model model) { // 게시글 전체조회
+		List<Board> list = boardService.getList();
+		model.addAttribute("list", list);
+		return "list";
+		
+	}
+	
+	@PostMapping("/register")
+	public String register(Board vo) {
+		boardService.register(vo);
+		return "redirect:/list";
+	}
+	
+	
+}
