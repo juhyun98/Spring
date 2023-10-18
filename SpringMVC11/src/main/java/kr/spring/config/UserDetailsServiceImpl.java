21ef62e327ba1344791354d6d82b753bf1aa772b
ojh <nbczerg98@naver.com>
2023-10-18 12:40:49 +0900

    Wed Oct 18 12:40:49     2023    by juhyun

--- SpringMVC11/src/main/java/kr/spring/config/UserDetailsServiceImpl.java
+++ SpringMVC11/src/main/java/kr/spring/config/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import kr.spring.entity.CustomUser;
 import kr.spring.entity.Member;
 import kr.spring.repository.MemberRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
